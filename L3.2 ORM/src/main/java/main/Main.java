8f5866f42410895eef2305a3c544bf4f84c2efdf
FLYLI\flyli <aver32@list.ru>
2024-03-29 17:24:28 +0500

    лабы 3 и 4

--- L3.2 ORM/src/main/java/main/Main.java
+++ L3.2 ORM/src/main/java/main/Main.java
@@ -1,9 +1,19 @@
 package main;
 
 
+import accounts.AccountService;
+import accounts.UserProfile;
 import dbService.DBException;
 import dbService.DBService;
 import dbService.dataSets.UsersDataSet;
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.SessionsServlet;
+import servlets.UsersServlet;
 
 /**
  * @author v.chibrikov
@@ -13,7 +23,28 @@ import dbService.dataSets.UsersDataSet;
  *         Описание курса и лицензия: https://github.com/vitaly-chibrikov/stepic_java_webserver
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        AccountService accountService = new AccountService();
+
+        accountService.addNewUser(new UserProfile("admin"));
+        accountService.addNewUser(new UserProfile("test"));
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(new UsersServlet(accountService)), "/signup");
+        context.addServlet(new ServletHolder(new SessionsServlet(accountService)), "/signin");
+
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setResourceBase("public_html");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{resource_handler, context});
+
+        Server server = new Server(8080);
+        server.setHandler(handlers);
+
+        server.start();
+        java.util.logging.Logger.getGlobal().info("Server started");
+        server.join();
         DBService dbService = new DBService();
         dbService.printConnectInfo();
 
